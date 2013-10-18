ef24dd7911a31b774a7fd38cdadfb8bf29b2df5f
velicanu <velicanu@mit.edu>
2013-10-18 18:44:02 -0400

    Update runcorr.sh
    bugfix, added | bash

--- runcorr.sh
+++ runcorr.sh
@@ -7,6 +7,7 @@ fi
 echo | awk -v i=$1 -v trkq=$2 -v flist=$3 -v tag=$4 -v nmin=$5 -v nmax=$6 -v pttrigmin=$7 -v pttrigmax=$8 -v ptassmin=$9 -v ptassmax=${10} '{print "./runcorr.exe "i" "trkq" "flist" "tag" "nmin" "nmax" "pttrigmin" "pttrigmax" "ptassmin" "ptassmax" "}' 
 echo | awk -v i=$1 -v trkq=$2 -v flist=$3 -v tag=$4 -v nmin=$5 -v nmax=$6 -v pttrigmin=$7 -v pttrigmax=$8 -v ptassmin=$9 -v ptassmax=${10} '{print "./runcorr.exe "i" "trkq" "flist" "tag" "nmin" "nmax" "pttrigmin" "pttrigmax" "ptassmin" "ptassmax" "}' | bash
 echo | awk -v tag=$4 -v user=$USER '{print "mkdir -p /net/hidsk0001/d00/scratch/"user"/corrhists/HIRun2013/unmerged"}'
+echo | awk -v tag=$4 -v user=$USER '{print "mkdir -p /net/hidsk0001/d00/scratch/"user"/corrhists/HIRun2013/unmerged"}' | bash
 echo | awk -v tag=$4 -v user=$USER '{print "mv "tag"*.root /net/hidsk0001/d00/scratch/"user"/corrhists/HIRun2013/unmerged"}'
 echo | awk -v tag=$4 -v user=$USER '{print "mv "tag"*.root /net/hidsk0001/d00/scratch/"user"/corrhists/HIRun2013/unmerged"}' | bash
 
