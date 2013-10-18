43520af55bd59d819e4ce769b3dbb80cd86cbed5
velicanu <velicanu@mit.edu>
2013-10-18 18:56:53 -0400

    Update submit.sh

--- submit.sh
+++ submit.sh
@@ -4,14 +4,14 @@ then
   exit 1
 fi
 
-now="$3_$(date +"%Y_%m_%d__%H_%M_%S")"
+now="$3_$4_$5_$6_$7_$8_$9_$(date +"%Y_%m_%d__%H_%M_%S")"
 len=`wc -l $2 | awk '{print $1}'`
 njobs=$((len))
 
 mkdir $now
 cp $2 $now
 cp runcorr.sh $now
-cat runcorr.condor | sed "s@log_flag@$now@g" | sed "s@dir_flag@$PWD/$now@g" |  sed "s@arglist@$1 $2 $3 $4 $5 $6 $7 $8 $9@g" | sed "s@transfer_filelist@$2,runcorr.exe@g" | sed "s@njobs@$njobs@g" > $now/runcorr.condor
+cat runcorr.condor | sed "s@log_flag@$now@g" | sed "s@dir_flag@$PWD/$now@g" | sed "s@user_flag@$USER@g" |  sed "s@arglist@$1 $2 $3 $4 $5 $6 $7 $8 $9@g" | sed "s@transfer_filelist@$2,runcorr.exe@g" | sed "s@njobs@$njobs@g" > $now/runcorr.condor
 
 NAME="runcorr.C"
 g++ $NAME $(root-config --cflags --libs) -Werror -Wall -O2 -o "${NAME/%.C/}.exe"
